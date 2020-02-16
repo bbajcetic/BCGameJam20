e32919f72b263dfd52792c863dc8b59e685440cc
Branko Bajcetic <bajcetic.branko@gmail.com>
2020-02-15 20:55:00 -0800

    Clean up client and server files a little

--- src/client.cpp
+++ src/client.cpp
@@ -15,83 +15,87 @@
 #include <stdbool.h>
 #include <fcntl.h>
 #include <time.h>
+#include "custom.h"
 
 #define SERVERPORT "4950"	// the port users will be connecting to
 #define MAXBUFLEN 100
 
-void wait(long seconds, long microseconds)
-{
-    struct timespec reqDelay = { seconds, microseconds * 1000};
-    nanosleep(&reqDelay, (struct timespec*) NULL);
-}
+struct networkInfo {
+    int sockfd;
+    struct addrinfo* netInfo;
+    char server_ip[20];
+};
+struct networkInfo connection;
 
+void connect() {
+	struct addrinfo *p, hints, *servinfo;
+    strcpy(connection.server_ip, "127.0.0.1");
+    //strcpy(connection.server_ip, "206.87.203.1");
 
-int main(int argc, char *argv[])
-{
-	int sockfd;
-	struct addrinfo hints, *servinfo, *p;
-	int rv;
-	int numbytes;
-	struct sockaddr_storage their_addr;
-	char buf[MAXBUFLEN];
-    //char* server_ip = "206.87.203.1";
-    char server_ip[] = "127.0.0.1";
-	socklen_t addr_len;
-	char s[INET6_ADDRSTRLEN];
-
-	memset(&hints, 0, sizeof hints);
+	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_DGRAM;
 
-	if ((rv = getaddrinfo(server_ip, SERVERPORT, &hints, &servinfo)) != 0) {
+    int rv;
+	if ((rv = getaddrinfo(connection.server_ip, SERVERPORT, &hints, &servinfo)) != 0) {
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-		return 1;
+		exit(1);
 	}
 
 	// loop through all the results and make a socket
 	for(p = servinfo; p != NULL; p = p->ai_next) {
-		if ((sockfd = socket(p->ai_family, p->ai_socktype,
+		if ((connection.sockfd = socket(p->ai_family, p->ai_socktype,
 				p->ai_protocol)) == -1) {
 			perror("talker: socket");
 			continue;
 		}
 
-        if ((rv = fcntl(sockfd, F_SETFL, O_NONBLOCK)) == -1) {
+        if ((rv = fcntl(connection.sockfd, F_SETFL, O_NONBLOCK)) == -1) {
 			perror("listener: fcntl");
 			continue;
         }
 
 		break;
 	}
+	freeaddrinfo(servinfo);
 
 	if (p == NULL) {
 		fprintf(stderr, "talker: failed to create socket\n");
-		return 2;
+		exit(2);
 	}
+    connection.netInfo = p;
+}
+int main(int argc, char *argv[])
+{
+    connect();
 
-    char first_msg[] = "connect with me\n";
-	if ((numbytes = sendto(sockfd, first_msg, strlen(first_msg), 0,
-			 p->ai_addr, p->ai_addrlen)) == -1) {
+    char first_msg[] = "client connect\n";
+	int numbytes;
+	if ((numbytes = sendto(connection.sockfd, first_msg, strlen(first_msg), 0,
+			 connection.netInfo->ai_addr, connection.netInfo->ai_addrlen)) == -1) {
 		perror("talker: sendto");
-		//exit(1);
 	}
 
     int msg_no = 0;
+	char buf[MAXBUFLEN];
+
+    char msg[1024];
+    strcpy(msg, "Hello from client ");
+    snprintf(msg+strlen(msg), 1024, "%d\n", msg_no);
+
     while (true) {
         msg_no++;
-        char msg[1024];
-        strcpy(msg, "Hello from client ");
-        snprintf(msg+strlen(msg), 1024, "%d\n", msg_no);
         printf("About to send: %s\n", msg);
 
-	    if ((numbytes = sendto(sockfd, msg, strlen(msg), 0,
-	    		 p->ai_addr, p->ai_addrlen)) == -1) {
+	    if ((numbytes = sendto(connection.sockfd, msg, strlen(msg), 0,
+	    		 connection.netInfo->ai_addr, connection.netInfo->ai_addrlen)) == -1) {
 	    	perror("talker: sendto");
 	    }
 	    printf("talker: sent %d bytes\n", numbytes);
 
-	    addr_len = sizeof their_addr;
-	    while ((numbytes = recvfrom(sockfd, buf, MAXBUFLEN-1 , 0, (struct sockaddr *)&their_addr, &addr_len)) != -1) {
+	    struct sockaddr_storage their_addr;
+	    socklen_t addr_len = sizeof(their_addr);
+	    while ((numbytes = recvfrom(connection.sockfd, buf, MAXBUFLEN-1 , 0, (struct sockaddr *)&their_addr, &addr_len)) != -1) {
 	        buf[numbytes] = '\0';
 	        printf("talker: packet = \"%s\"\n", buf);
 	    }
@@ -103,8 +107,7 @@ int main(int argc, char *argv[])
 
     }
 
-	freeaddrinfo(servinfo);
-	close(sockfd);
+	close(connection.sockfd);
 
 	return 0;
 }
--- src/server.cpp
+++ src/server.cpp
@@ -15,113 +15,99 @@
 #include <stdbool.h>
 #include <fcntl.h>
 #include <time.h>
+#include "custom.h"
 
 #define MYPORT "4950"	// the port users will be connecting to
 
 #define MAXBUFLEN 100
 
-// get sockaddr, IPv4 or IPv6:
-void *get_in_addr(struct sockaddr *sa)
-{
-	if (sa->sa_family == AF_INET) {
-		return &(((struct sockaddr_in*)sa)->sin_addr);
-	}
+struct networkInfo {
+    int sockfd;
+    struct addrinfo* netInfo;
+    struct sockaddr_storage client_addr;
+};
+struct networkInfo connection;
 
-	return &(((struct sockaddr_in6*)sa)->sin6_addr);
-}
-
-void wait(long seconds, long microseconds)
-{
-    struct timespec reqDelay = { seconds, microseconds * 1000};
-    nanosleep(&reqDelay, (struct timespec*) NULL);
-}
-
-
-
-int main(void)
-{
-	int sockfd;
-	struct addrinfo hints, *servinfo, *p;
-	int rv;
-	int numbytes;
-	struct sockaddr_storage their_addr;
-	char buf[MAXBUFLEN];
-	socklen_t addr_len;
-	char s[INET6_ADDRSTRLEN];
+void connect() {
+	struct addrinfo *p, hints, *servinfo;
 
 	memset(&hints, 0, sizeof hints);
 	hints.ai_family = AF_UNSPEC; // set to AF_INET to force IPv4
 	hints.ai_socktype = SOCK_DGRAM;
 	hints.ai_flags = AI_PASSIVE; // use my IP
 
-	//if ((rv = getaddrinfo("127.0.0.1", MYPORT, &hints, &servinfo)) != 0) {
+	int rv;
 	if ((rv = getaddrinfo(NULL, MYPORT, &hints, &servinfo)) != 0) {
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-		return 1;
+		exit(1);
 	}
 
 	// loop through all the results and bind to the first we can
 	for(p = servinfo; p != NULL; p = p->ai_next) {
-		if ((sockfd = socket(p->ai_family, p->ai_socktype,
+		if ((connection.sockfd = socket(p->ai_family, p->ai_socktype,
 				p->ai_protocol)) == -1) {
 			perror("listener: socket");
 			continue;
 		}
-        if ((rv = fcntl(sockfd, F_SETFL, O_NONBLOCK)) == -1) {
+        if ((rv = fcntl(connection.sockfd, F_SETFL, O_NONBLOCK)) == -1) {
 			perror("listener: fcntl");
 			continue;
         }
 
-		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-			close(sockfd);
+		if (bind(connection.sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			close(connection.sockfd);
 			perror("listener: bind");
 			continue;
 		}
 
 		break;
 	}
+	freeaddrinfo(servinfo);
 
 	if (p == NULL) {
 		fprintf(stderr, "listener: failed to bind socket\n");
-		return 2;
+		exit(2);
 	}
+}
 
-	freeaddrinfo(servinfo);
+int main(void)
+{
+    connect();
 
-    addr_len = sizeof(their_addr);
+	char buf[MAXBUFLEN];
+
+	socklen_t addr_len = sizeof(connection.client_addr);
     //get client's address info
+	int numbytes;
     do {
-        numbytes = recvfrom(sockfd, buf, MAXBUFLEN-1 , 0, (struct sockaddr *)&their_addr, &addr_len);
+        numbytes = recvfrom(connection.sockfd, buf, MAXBUFLEN-1 , 0, (struct sockaddr *)&connection.client_addr, &addr_len);
     } while(numbytes == -1);
 
     int msg_no = 0;
+
+    char msg[1024];
+    strcpy(msg, "Hello from server ");
+    snprintf(msg+strlen(msg), 1024, "%d\n", msg_no);
+
     while (true) {
 	    struct sockaddr_storage temp;
         socklen_t temp_len;
 
         msg_no++;
-        char msg[1024];
-        strcpy(msg, "Hello from server ");
-        snprintf(msg+strlen(msg), 1024, "%d\n", msg_no);
         printf("About to send: %s\n", msg);
 
-	    while ((numbytes = recvfrom(sockfd, buf, MAXBUFLEN-1 , 0, (struct sockaddr *)&temp, &temp_len)) != -1) {
-	        printf("listener: got packet from %s\n", inet_ntop(their_addr.ss_family, 
-                        get_in_addr((struct sockaddr *)&their_addr), s, sizeof s));
-	        printf("listener: packet is %d bytes long\n", numbytes);
+	    while ((numbytes = recvfrom(connection.sockfd, buf, MAXBUFLEN-1 , 0, (struct sockaddr *)&temp, &temp_len)) != -1) {
 	        buf[numbytes] = '\0';
 	        printf("listener: packet contains \"%s\"\n", buf);
 	    }
         if (errno != EAGAIN && errno != EWOULDBLOCK) {
 	    	perror("recvfrom");
-	    	//exit(1);
         }
 
 
-	    if ((numbytes = sendto(sockfd, msg, strlen(msg), 0,
-	    		 (const struct sockaddr *)&their_addr, sizeof(their_addr)/*p->ai_addr, p->ai_addrlen*/)) == -1) {
+	    if ((numbytes = sendto(connection.sockfd, msg, strlen(msg), 0,
+	    		 (const struct sockaddr *)&connection.client_addr, sizeof(connection.client_addr))) == -1) {
 	    	perror("listener: sendto");
-	    	//exit(1);
 	    }
 
 	    printf("listener: sent %d bytes\n", numbytes);
@@ -130,7 +116,7 @@ int main(void)
     }
 
 
-	close(sockfd);
+	close(connection.sockfd);
 
 	return 0;
 }
